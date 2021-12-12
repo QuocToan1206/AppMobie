77f951de6178c7ef91da1ee025f437768501c3d4
Player <49013524+toanlqctk42@users.noreply.github.com>
2021-12-12 17:45:35 +0700

    update

--- pacmangame/lib/homepage.dart
+++ pacmangame/lib/homepage.dart
@@ -14,7 +14,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   static int numberInRow = 11;
-  int numberOfSquares = numberInRow * 17;
+  int numberOfSquares = numberInRow * 16;
   int nv = numberInRow * 14 + 1;
 
   List<int> barriers = [
@@ -117,13 +117,8 @@ class _HomePageState extends State<HomePage> {
   ];
   List<int> foods = [];
 
-<<<<<<< HEAD
   void startGame() {
     getFood();
-=======
-  //bắt đầu
-  void startGame(){
->>>>>>> 3310fa950b44eedf40992eb9fdde4cf7177812b2
     Timer.periodic(Duration(milliseconds: 300), (timer) {
       if (foods.contains(nv)) {
         foods.remove(nv);
@@ -180,7 +175,7 @@ class _HomePageState extends State<HomePage> {
                         return Padding(
                             padding: const EdgeInsets.all(1.0),
                             child: Container(
-                                color: Colors.black,
+                                color: Colors.yellow,
                                 child: Text(index.toString())));
                       }
                     }),
@@ -188,7 +183,6 @@ class _HomePageState extends State<HomePage> {
           //Điểm với play
           Expanded(
               child: Container(
-<<<<<<< HEAD
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -205,18 +199,6 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ))
-=======
-                child: Row(
-                  mainAxisAlignment:  MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text("Điểm: ",style: TextStyle(color: Colors.white, fontSize: 40),),
-                    GestureDetector(
-                        onTap: startGame ,//nhấn vào chữ bắt đầu để chạy lệnh
-                        child: Text("P L A Y ", style: TextStyle(color: Colors.white, fontSize: 40),)),
-                  ],
-                  ),
-              ))
->>>>>>> 3310fa950b44eedf40992eb9fdde4cf7177812b2
         ],
       ),
     );
